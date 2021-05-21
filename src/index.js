6f885f1e3a2f596759324c86d2b5523f39a15f6e
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-05-21 23:19:50 +0100

    added logic to print table of departments on view all departments option

--- src/index.js
+++ src/index.js
@@ -8,87 +8,105 @@ const init = async () => {
 
   await db.start();
 
-  const optionsQuestion = {
-    type: "list",
-    message: "What would you like to do?",
-    name: "option",
-    choices: [
-      {
-        short: "All Employees",
-        value: "viewAllEmployees",
-        name: "View All Employees",
-      },
-      {
-        short: "Employees By Department",
-        value: "viewAllByDepartment",
-        name: "View All Employees By Department",
-      },
-      {
-        short: "Employees By Manager",
-        value: "viewAllByManager",
-        name: "View All Employees By Manager",
-      },
-      {
-        value: "addEmployee",
-        name: "Add Employee",
-      },
-      {
-        value: "deleteEmployee",
-        name: "Delete Employee",
-      },
-      {
-        short: "Employee Role",
-        value: "updateRole",
-        name: "Update Employee Role",
-      },
-      {
-        short: "Employee Manager",
-        value: "updateManager",
-        name: "Update Employee Manager",
-      },
-      {
-        short: "All Roles",
-        value: "allRoles",
-        name: "View All Roles",
-      },
-      {
-        value: "addRole",
-        name: "Add Role",
-      },
-      {
-        value: "removeRole",
-        name: "Remove Role",
-      },
-      {
-        short: "All Departments",
-        value: "allDepartments",
-        name: "View All Departments",
-      },
-      {
-        value: "addDepartment",
-        name: "Add Department",
-      },
-      {
-        value: "removeDepartment",
-        name: "Remove Department",
-      },
-      {
-        short: "Department Spend",
-        value: "departmentSpend",
-        name: "View Total Utilised Budget of a Department",
-      },
-      {
-        short: "Exit",
-        value: "EXIT",
-        name: "End App",
-      },
-    ],
-  };
+  let inProgress = true;
 
-  const { option } = await inquirer.prompt(optionsQuestion);
-  console.log(option);
+  while (inProgress) {
+    const optionsQuestion = {
+      type: "list",
+      message: "What would you like to do?",
+      name: "option",
+      choices: [
+        {
+          short: "All Employees",
+          value: "viewAllEmployees",
+          name: "View All Employees",
+        },
+        {
+          short: "Employees By Department",
+          value: "viewAllByDepartment",
+          name: "View All Employees By Department",
+        },
+        {
+          short: "Employees By Manager",
+          value: "viewAllByManager",
+          name: "View All Employees By Manager",
+        },
+        {
+          value: "addEmployee",
+          name: "Add Employee",
+        },
+        {
+          value: "deleteEmployee",
+          name: "Delete Employee",
+        },
+        {
+          short: "Employee Role",
+          value: "updateRole",
+          name: "Update Employee Role",
+        },
+        {
+          short: "Employee Manager",
+          value: "updateManager",
+          name: "Update Employee Manager",
+        },
+        {
+          short: "All Roles",
+          value: "allRoles",
+          name: "View All Roles",
+        },
+        {
+          value: "addRole",
+          name: "Add Role",
+        },
+        {
+          value: "removeRole",
+          name: "Remove Role",
+        },
+        {
+          short: "All Departments",
+          value: "allDepartments",
+          name: "View All Departments",
+        },
+        {
+          value: "addDepartment",
+          name: "Add Department",
+        },
+        {
+          value: "removeDepartment",
+          name: "Remove Department",
+        },
+        {
+          short: "Department Spend",
+          value: "departmentSpend",
+          name: "View Total Utilised Budget of a Department",
+        },
+        {
+          short: "Exit",
+          value: "EXIT",
+          name: "EXIT APP",
+        },
+      ],
+    };
 
-  db.end("Thank you for using our app");
+    const { option } = await inquirer.prompt(optionsQuestion);
+
+    if (option === "allDepartments") {
+      const allDepartments = await db.selectAll("department");
+      const departmentNames = allDepartments.map((each) => {
+        return {
+          department: each.name,
+        };
+      });
+
+      const table = cTable.getTable(departmentNames);
+      console.log(table);
+    }
+
+    if (option === "EXIT") {
+      inProgress = false;
+      db.end("Thank you for using our app");
+    }
+  }
 };
 
 init();
