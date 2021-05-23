cd10930ff1270c7d570a90b47f37658f00c1245e
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-05-23 18:28:27 +0100

    refactored util function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,4 +6,22 @@ LEFT JOIN ?? ON ?? = ??
 `;
 };
 
-module.exports = { leftJoin };
+const generateEmployeeChoices = (employees) => {
+  return employees.map((employee) => {
+    return {
+      name: `${employee.first_name} ${employee.last_name}`,
+      value: employee.id,
+    };
+  });
+};
+
+const generateRoleChoices = (roles) => {
+  return roles.map((role) => {
+    return {
+      name: role.title,
+      value: role.id,
+    };
+  });
+};
+
+module.exports = { leftJoin, generateEmployeeChoices, generateRoleChoices };
